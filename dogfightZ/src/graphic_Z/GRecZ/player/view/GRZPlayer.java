2dc28f9336e24343682f308cf7e2b5dc379e8a99
兰彦真 <1421023950@qq.com>
2022-05-26 10:49:24 +0800

    在游戏录制播放功能中增加了屏幕缩放和字体切换的支持

--- dogfightZ/src/graphic_Z/GRecZ/player/view/GRZPlayer.java
+++ dogfightZ/src/graphic_Z/GRecZ/player/view/GRZPlayer.java
@@ -6,4 +6,10 @@ public interface GRZPlayer {
     PController getController();
     void setVisible(boolean b);
     void dispose();
+    void decSize();
+    void addSize();
+    void privFont();
+    void nextFont();
+    void switchFont(int idx);
+    void setScrZoom(int size);
 }
